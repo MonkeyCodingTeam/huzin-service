bdce65117988507596085777e6dffbadadd7d2ff
Zaripov Adel <zaripovar1998@gmail.com>
2023-06-19 13:26:27 +0300

    change telegram command settings

--- src/pages/Target/SettingsPage/ui/ClientSettings/ui/ClientSettings.tsx
+++ src/pages/Target/SettingsPage/ui/ClientSettings/ui/ClientSettings.tsx
@@ -1,7 +1,7 @@
 import { ListBox, ListBoxChangeEvent } from 'primereact/listbox';
 import { selectClient } from '@entities/client/model';
 import { useAppDispatch, useAppSelector } from '@shared/lib/redux/hooks';
-import { MouseEvent, useEffect, useRef, useState } from 'react';
+import { MouseEvent, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router';
 import { ROUTES } from '@shared/const/routes';
 import { ClientAPI } from '@shared/lib/api';
@@ -13,6 +13,8 @@ import { ClientSettingsGroup } from '../../ClientSettingsGroup';
 import { Button } from 'primereact/button';
 import { Tag } from 'primereact/tag';
 import { PrimeIcons } from 'primereact/api';
+import { InputText } from 'primereact/inputtext';
+import classNames from 'classnames';
 
 export const ClientSettings = () => {
   const [clients, setClients] = useState<Client[]>([]);
@@ -20,7 +22,6 @@ export const ClientSettings = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const params = useParams<{ clientId: string }>();
-  const tgCommandButton = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     ClientAPI.getClients().then((res) => {
@@ -46,19 +47,14 @@ export const ClientSettings = () => {
     }
   };
 
-  const copyTelegramCommand = (e: MouseEvent) => {
-    const { current } = tgCommandButton;
-    if (!current) return;
+  const copyTelegramCommand = (e: MouseEvent<HTMLButtonElement>) => {
+    const command = `/register ${selectedClient.id}`;
+    const { currentTarget } = e;
+    currentTarget.classList.add('p-button-success');
 
-    const text = current.innerText;
-    current.classList.add('p-button-success');
-    current.innerText = 'Скопировано';
-
-    navigator.clipboard.writeText(text).then(() => {
-      // e.currentTarget.
+    navigator.clipboard.writeText(command).then(() => {
       setTimeout(() => {
-        current.innerText = text;
-        current.classList.remove('p-button-success');
+        currentTarget.classList.remove('p-button-success');
       }, 2000);
     });
   };
@@ -113,7 +109,6 @@ export const ClientSettings = () => {
                 </Divider>
                 <ClientSettingsGroup client={selectedClient} />
               </div>
-
               <Divider id='telegram' align='left'>
                 <a href='#telegram' className={css.settings__list__anchor}>
                   # Telegram
@@ -121,14 +116,11 @@ export const ClientSettings = () => {
               </Divider>
               <div>
                 <div className={css.telegramBlock}>
-                  <span className={css.telegramBlock__label}>Ренистрация клиента:</span>
-                  <Button
-                    //@ts-ignore
-                    ref={tgCommandButton}
-                    label={`/register ${selectedClient.id}`}
-                    outlined
-                    onClick={copyTelegramCommand}
-                  />
+                  <span className={css.telegramBlock__label}>Регистрация клиента:</span>
+                  <div className={classNames('p-inputgroup', css.telegramBlock__command)}>
+                    <Button icon={PrimeIcons.COPY} size='small' onClick={copyTelegramCommand} />
+                    <InputText readOnly value={`/register ${selectedClient.id}`} size='small' />
+                  </div>
                   {selectedClient.has_telegram ? (
                     <Tag severity='success' value='Есть чат' />
                   ) : (
@@ -136,113 +128,6 @@ export const ClientSettings = () => {
                   )}
                 </div>
               </div>
-              <Divider id='tags' align='left'>
-                <a href='#tags' className={css.settings__list__anchor}>
-                  # Теги
-                </a>
-              </Divider>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi cupiditate delectus
-                facere fuga illo in labore laboriosam maxime necessitatibus nobis odio porro quasi
-                quidem, quis repellat saepe sapiente tempore, veritatis.
-              </p>
-              <p>
-                Aliquam architecto beatae commodi consequuntur dolore libero perspiciatis soluta
-                voluptas. Asperiores laborum molestiae possimus. Accusamus cum delectus deserunt,
-                doloribus, ipsa, laudantium perferendis praesentium quasi qui quia reiciendis rem
-                vel voluptas.
-              </p>
-              <p>
-                Consectetur earum explicabo facere, magni maiores nihil nobis optio praesentium
-                ratione reprehenderit sunt temporibus ut voluptas! Ab architecto est expedita
-                explicabo hic ipsam, iusto quia quisquam, sequi tempore temporibus voluptas!
-              </p>
-              <p>
-                Culpa, optio, voluptas! At blanditiis fugit magnam perspiciatis quasi. Accusamus
-                assumenda consequatur, dolore doloremque ducimus eum eveniet in ipsa itaque,
-                laboriosam nam necessitatibus nemo odit quasi rem sequi ullam, veniam?
-              </p>
-              <p>
-                A ad architecto aspernatur at, consequuntur corporis cum, deserunt distinctio dolore
-                eaque eveniet facilis harum ipsa iusto labore minima nam nobis odit possimus
-                provident quasi quis sapiente, suscipit tempore veniam?
-              </p>
-              <p>
-                A ab accusamus adipisci aliquam amet aut cupiditate delectus deserunt ducimus,
-                facilis itaque repudiandae, saepe velit! Amet consequatur, corporis debitis
-                doloribus in maxime nostrum officia quia quis quod soluta veritatis.
-              </p>
-              <p>
-                A ad architecto assumenda dignissimos fugit illo ipsa iure laboriosam molestiae
-                nemo, porro sed, similique soluta ut voluptatem? Aliquid animi facilis natus
-                provident quam reiciendis sunt suscipit! Ab, culpa, cupiditate.
-              </p>
-              <p>
-                Atque cupiditate maiores quibusdam? Ab amet at commodi consectetur distinctio,
-                dolore dolorum et exercitationem facilis harum in itaque nesciunt nihil officiis
-                omnis qui quo recusandae reprehenderit sequi suscipit velit voluptate?
-              </p>
-              <p>
-                A accusamus aperiam asperiores aut autem ea eaque esse eum facilis iste iusto libero
-                magni minus placeat quaerat quis quisquam quo ratione recusandae repellendus,
-                repudiandae saepe suscipit temporibus, vel, voluptas?
-              </p>
-              <p>
-                Accusamus animi autem consequuntur cum debitis enim esse eveniet ex hic magnam
-                maiores molestias neque nisi nobis pariatur perspiciatis, quidem quis ratione
-                repellat sit, ut vel voluptate. Amet, ducimus iure?
-              </p>
-              <p>
-                Adipisci aliquam culpa cum cumque excepturi incidunt nemo officiis quam repudiandae
-                saepe sapiente, sequi ullam unde. Aliquam consectetur cum facere impedit porro, qui
-                sequi sint tempora velit. Debitis, doloribus illo?
-              </p>
-              <p>
-                Eveniet quae quia voluptatum? Ab aliquam architecto autem corporis deleniti dolore
-                eligendi error facilis iusto laborum molestias nam non numquam pariatur possimus
-                quas reprehenderit, sit soluta sunt unde velit veniam.
-              </p>
-              <p>
-                Accusamus aliquid architecto deleniti deserunt eos excepturi facere incidunt ipsam
-                modi mollitia praesentium repellendus suscipit tenetur, voluptas voluptatum? Animi
-                deserunt doloremque dolorum ea error fugit necessitatibus optio quibusdam rerum
-                tempore.
-              </p>
-              <p>
-                Accusamus aut commodi eum incidunt inventore molestias neque, nobis non nulla
-                obcaecati, omnis, perspiciatis quam quod rem sint sunt tempore voluptatum.
-                Doloremque ea iure numquam odit qui ratione ullam veniam.
-              </p>
-              <p>
-                Consequatur ducimus illum iste libero, modi nulla odit pariatur quaerat qui rerum.
-                Adipisci corporis cupiditate delectus enim, inventore magni maxime neque nobis
-                officia, quaerat repudiandae sed soluta voluptate! Dicta, soluta.
-              </p>
-              <p>
-                Accusamus amet corporis culpa dignissimos eius est et explicabo facilis fuga illum
-                iure laboriosam laudantium magnam maiores minima nam quaerat quas quis, quod
-                repellat sequi ullam velit vero. Ad, earum?
-              </p>
-              <p>
-                Amet maiores nihil qui quidem tempora? Alias aliquid animi at beatae consequuntur
-                culpa delectus distinctio dolorum eveniet ex explicabo, id modi molestiae, mollitia
-                natus nemo, obcaecati qui quibusdam soluta voluptate!
-              </p>
-              <p>
-                Esse fugit tempora ut voluptatibus. A, deserunt dolore ea excepturi impedit ipsum
-                nihil officia porro quo repellendus reprehenderit sit velit voluptates. Dignissimos
-                iusto laboriosam, laudantium nam perspiciatis placeat veritatis. Hic.
-              </p>
-              <p>
-                Itaque, quis, recusandae. Aperiam consectetur dolores earum eligendi fugiat, id
-                itaque officia praesentium. Hic natus nisi omnis vero! Autem eos fugiat hic illo
-                nemo neque nostrum quae quos unde vero!
-              </p>
-              <p>
-                A consectetur deleniti eius, ex fugiat incidunt mollitia nam, nemo perferendis qui
-                quis temporibus ullam voluptates! Consequuntur enim et eum exercitationem explicabo
-                iure laboriosam ullam vero voluptatum! Nostrum, quam, rem.
-              </p>
             </>
           )}
         </div>
@@ -253,9 +138,6 @@ export const ClientSettings = () => {
           </a>
           <a href='#telegram' className={css.settings__panel__anchor}>
             # Telegram
-          </a>
-          <a href='#tags' className={css.settings__panel__anchor}>
-            # Теги
           </a>
         </div>
       </div>
