7943d57f8bd1f59558f6cb52e08685884f142942
Zaripov Adel <zaripovar1998@gmail.com>
2023-07-06 21:17:06 +0300

    add stories card

--- src/pages/Content/StoriesPage/ui/AddStoriesDialog/ui/AddStoriesDialog.tsx
+++ src/pages/Content/StoriesPage/ui/AddStoriesDialog/ui/AddStoriesDialog.tsx
@@ -140,7 +140,6 @@ export const AddStoriesDialog: FC<AddStoriesDialogProps> = ({
     const { file } = story;
 
     if (!file) return;
-
     const url = window.URL.createObjectURL(file);
 
     return (
